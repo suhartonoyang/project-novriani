ecf02861ee07f94607684f50a90dc3b79a5b12a6
Suhartono <suhartonoyang@gmail.com>
2021-05-26 16:34:44 +0700

    Initial Project
    initial project

---
+++ src/test/java/com/project/novriani/NovrianiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.novriani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovrianiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
